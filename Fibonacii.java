aa17ece4151959c1c42223744e0a04be9c2c6b8c
AMRITESH ANAND <115371712+amriteshanand0129@users.noreply.github.com>
2022-12-04 12:49:47 +0530

    Update Fibonacii.java

--- Fibonacii.java
+++ Fibonacii.java
@@ -10,19 +10,14 @@ public class Fibonacii
         int b = 1;
         System.out.println(a);
         System.out.println(b);
-        int s=0, c=0, sum=0;
+        int s=0, c=0;
         while(s<n){
             s=a+b;
             if(s>n)
                 break;
             System.out.println(s);
-            if(c%2==0)
-                sum=sum+s;
             a=b;
             b=s;
-            c++;
         }
-        System.out.println("sum="+ sum);
     }
-    
 }
