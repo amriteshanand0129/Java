d18ef590a18f2e4cadee8c44a15775c11b801e2b
AMRITESH ANAND_12218090 <115371712+amriteshanand0129@users.noreply.github.com>
2022-12-29 15:56:29 +0530

    Update Spiralmatrix.java

--- Spiralmatrix.java
+++ Spiralmatrix.java
@@ -19,42 +19,10 @@ public class Spiralmatrix {
                 System.out.print(matrix[i][j]+" ");
             }System.out.println();
         }
-        //System.out.print(matrix[0][0]+" ");
         int rowmax=r-1, colmax=c-1, rowmin=0, colmin=0, row=0, col=0;
-        /*for(int i=1;i<=(2*r-1);i++){
-            x++;
-            if(row==rowmax){
-                col--;
-                while(col>=colmin){
-                    System.out.print(matrix[row][col]+" ");
-                    col--;
-                }rowmax--;col++;
-            }
-            else if(col==colmin && row!=rowmin){
-                row--;
-                while(row>=rowmin){
-                    System.out.print(matrix[row][col]+" ");
-                    row--;
-                }colmin++;row++;
-            }
-            else if(row==rowmin){
-                col++;
-                while(col<=colmax){
-                    System.out.print(matrix[row][col]+" ");
-                    col++;
-                }rowmin++;col--;
-            }
-            else if(col==colmax){
-                row++; 
-                while(row<=rowmax){
-                    System.out.print(matrix[row][col]+" ");
-                    row++;
-                }colmax--;row--;
-            }
-        }*/
         // The condition in while loop ensures complete printing of matrix of even odd order.
         // The condition of count++ of mine would not work for some odd order matrices.
-        // Moreover if conditio in 2nd and 3rd loop if for mxn order matrices where either row/column is greater.
+        // Moreover if condition in 2nd and 3rd loop if for mxn order matrices where either row/column is greater.
         while(rowmin<=rowmax && colmin<=colmax){
             row = rowmin;
             for(int i=colmin;i<=colmax;i++){
