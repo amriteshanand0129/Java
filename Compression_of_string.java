9240db8fe995b5b62699d55040698d60c048f7fa
AMRITESH ANAND_12218090 <115371712+amriteshanand0129@users.noreply.github.com>
2022-12-29 15:59:38 +0530

    Update and rename Direction.java to Compression_of_string.java

--- Compression_of_string.java
+++ Compression_of_string.java
@@ -2,28 +2,6 @@ import java.util.*;
 class Direction{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
-
-        //Calculate displacement of coordinate
-        
-        System.out.println("Enter the direction");
-        String dir = in.nextLine();
-        int x = 0;
-        int y = 0;
-        for(int i=0;i<dir.length();i++){
-            char ch = dir.charAt(i);
-            if(ch=='E')
-                x++;
-            else if(ch=='N')
-                y++;
-            else if(ch=='W')
-                x--;
-            else if(ch=='S')
-                y--;
-        }
-        System.out.println("Final cooridnates="+(x)+","+(y));
-        double displacement = Math.sqrt(x*x+y*y);
-        System.out.println("Displacement="+displacement);
-
         //Compression of string
 
         System.out.println("Enter string to be compressed");
